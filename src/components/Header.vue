90c1e435727c24d72d2f8327a6323213eb579d37
Haril Song <songkg7@gmail.com>
2021-08-08 01:50:47 +0900

    feat: Header - logo and fonts

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,6 @@
 <template>
   <header>
+    <Logo />
     <div class="nav nav-pills">
       <div v-for="nav in navigations" :key="nav.name" class="nav-item">
         <RouterLink :to="nav.href" active-class="active" class="nav-link">
@@ -11,7 +12,11 @@
 </template>
 
 <script>
+import Logo from "~/components/Logo";
 export default {
+  components: {
+    Logo,
+  },
   data() {
     return {
       navigations: [
@@ -32,3 +37,15 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+header {
+  height: 70px;
+  padding: 0 40px;
+  display: flex;
+  align-items: center;
+  .logo {
+    margin-right: 40px;
+  }
+}
+</style>
