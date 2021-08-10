fb8c3a5851025eef10fa508943fa8a9248c46cc9
Haril Song <songkg7@gmail.com>
2021-08-10 18:17:44 +0900

    style: skeleton ui

--- src/routes/Movie.vue
+++ src/routes/Movie.vue
@@ -1,5 +1,19 @@
 <template>
-  <h1>Movie!</h1>
+  <div class='container'>
+    <div class='skeletons'>
+      <div class='skeleton poster'>
+
+      </div>
+      <div class='specs'>
+        <div class='skeleton title'></div>
+        <div class='skeleton spec'></div>
+        <div class='skeleton plot'></div>
+        <div class='skeleton etc'></div>
+        <div class='skeleton etc'></div>
+        <div class='skeleton etc'></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,6 +24,53 @@ export default {
       // movie/{id}
       id: this.$route.params.id,
     })
-  }
+  },
 }
 </script>
+
+<style lang='scss' scoped>
+@import "~/scss/main";
+
+.container {
+  padding-top: 40px;
+}
+
+.skeletons {
+  display: flex;
+
+  .poster {
+    width: 500px;
+    height: 500px * 3/2;
+    margin-right: 70px;
+    flex-shrink: 0;
+  }
+
+  .specs {
+    flex-grow: 1;
+  }
+
+  .skeleton {
+    border-radius: 10px;
+    background-color: $gray-200;
+    &.title {
+      width: 80%;
+      height: 70px;
+    }
+    &.spec {
+      width: 60%;
+      height: 30px;
+      margin-top: 20px;
+    }
+    &.plot {
+      width: 100%;
+      height: 250px;
+      margin-top: 20px;
+    }
+    &.etc {
+      width: 50%;
+      height: 50px;
+      margin-top: 20px;
+    }
+  }
+}
+</style>
