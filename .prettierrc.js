4425f208bc1a4e73993cfc667544bda878ee55d0
Haril Song <songkg7@gmail.com>
2021-08-13 05:19:34 +0900

    setting: modify prettier setting

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  bracketSpacing: false, // 객체 리터럴의 괄호 사이의 공간
+  // bracketSpacing: false, // 객체 리터럴의 괄호 사이의 공간
   jsxBracketSameLine: true, // JSX 요소의 마지막 라인에 '>' 특수기호를 같은 라인|다음 라인
   singleQuote: true, // 쿼테이션 마크를 1개|2개
   trailingComma: 'all', // 끝에 ','를 붙임('es5'(기본), 'none', 'all' 3개의 옵션)
