9b15cf1c6de9185e08a9609840356f5662f99f2b
HarilSong <songkg7@gmail.com>
2021-08-15 03:17:13 +0900

    chore: local 및 서버의 환경 변수 구성

--- functions/movie.js
+++ functions/movie.js
@@ -1,10 +1,10 @@
 const axios = require('axios')
+const { OMDB_API_KEY } = process.env
 
 exports.handler = async function(event) {
   console.log(event)
   const payload = JSON.parse(event.body)
   const { title, type, page, year, id } = payload
-  const OMDB_API_KEY = 'cdda0f7a'
   const url = id
     ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}`
     : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type${type}&y=${year}&page=${page}`
