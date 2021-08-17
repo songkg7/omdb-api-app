afd415fa3ae5c82ad040875686a05b80ff89d65d
Haril Song <songkg7@gmail.com>
2021-08-18 03:32:56 +0900

    test: 비동기 테스트

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,21 +1,23 @@
-const userA = {
-  name: 'HARIL',
-  age: 27,
-}
-const userB = {
-  name: 'Neo',
-  age: 22,
-}
+import { asyncFn } from './example'
 
-test('데이터가 일치해야 합니다.', () => {
-  expect(userA.age).toBe(27)
-  expect(userA).toEqual({
-    name: 'HARIL',
-    age: 27,
+describe('비동기 테스트', () => {
+  test('done', (done) => {
+    asyncFn().then((res) => {
+      expect(res).toBe('Done!')
+      done()
+    })
   })
-})
 
-test('데이터가 일치하지 않아야 합니다.', () => {
-  expect(userB.name).not.toBe('HARIL')
-  expect(userB).not.toEqual(userA)
+  test('then', () => {
+    return asyncFn().then((res) => {
+      expect(res).toBe('Done!')
+    })
+  })
+
+  test('resolves', () => expect(asyncFn()).resolves.toBe('Done!'))
+
+  test('async/await', async () => {
+    const res = await asyncFn()
+    expect(res).toBe('Done!')
+  }, 7000)  // 기본값은 5000 이기 때문에 그 이상의 시간이 필요하다면 인자를 주어 설정할 수 있다.
 })
