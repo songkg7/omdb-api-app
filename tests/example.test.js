bb53ce57bf9309e446dff00c5b3627c0a5e7f828
Haril Song <songkg7@gmail.com>
2021-08-18 04:04:55 +0900

    test: mock 예제 1

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,23 +1,9 @@
-import { asyncFn } from './example'
+import * as example from './example'
 
 describe('비동기 테스트', () => {
-  test('done', (done) => {
-    asyncFn().then((res) => {
-      expect(res).toBe('Done!')
-      done()
-    })
-  })
-
-  test('then', () => {
-    return asyncFn().then((res) => {
-      expect(res).toBe('Done!')
-    })
-  })
-
-  test('resolves', () => expect(asyncFn()).resolves.toBe('Done!'))
-
   test('async/await', async () => {
-    const res = await asyncFn()
+    jest.spyOn(example, 'asyncFn').mockResolvedValue('Done!') // 모의함수(Mock)
+    const res = await example.asyncFn()
     expect(res).toBe('Done!')
-  }, 7000)  // 기본값은 5000 이기 때문에 그 이상의 시간이 필요하다면 인자를 주어 설정할 수 있다.
+  }, 7000)
 })
